d5304b8dd86a35568a271ac2b67f808d347b25b5
ccryptoDev <smartwebdev90@gmail.com>
2022-02-11 13:25:41 -0400

    add packages

---
+++ src/redux/reducers/templateConfig/index.jsx
@@ -0,0 +1,12 @@
+import options from './configOptions'
+
+const Configur = (state = options, action) => {
+  switch (action.type) {
+    case "LAYOUT": return {
+      ...state,
+    };
+    default: return state;
+  }
+}
+
+export default Configur
